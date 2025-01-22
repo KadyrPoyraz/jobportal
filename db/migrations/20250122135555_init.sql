f8351f889260aea4c039fb31d8b8add01d0a151c
salmon882 <orunalievsolomonnn@gmail.com>
2025-01-22 15:25:37 +0600

    fixed formatting in migration

--- db/migrations/20250122135555_init.sql
+++ db/migrations/20250122135555_init.sql
@@ -1,11 +1,11 @@
 -- +goose Up
 CREATE TABLE users
 (
-    id            BIGSERIAL     NOT NULL PRIMARY KEY,
-    first_name      VARCHAR(255),
-    last_name      VARCHAR(255),
-    email         VARCHAR(255)        NOT NULL UNIQUE,
-    password VARCHAR(255)
+    id         BIGSERIAL NOT NULL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name  VARCHAR(255),
+    email      VARCHAR(255) NOT NULL UNIQUE,
+    password   VARCHAR(255)
 );
 
 -- +goose Down
