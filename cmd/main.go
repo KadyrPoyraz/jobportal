43b961ee63b791c6b30e5df7a7071529c5edb8dd
KadyrPoyraz <kadyrpoyraz@gmail.com>
2025-01-22 13:58:36 +0600

    project init

---
+++ cmd/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/KadyrPoyraz/jobportal/handlers"
+	"github.com/KadyrPoyraz/jobportal/server"
+)
+
+func main() {
+	s := server.NewServer(8080)
+
+	s.Use(server.LoggingMiddleware)
+	s.Use(server.RecoveryMiddleware)
+
+	s.Router.GET("/", handlers.HomeHandler)
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		if err := s.Run(); err != nil {
+			log.Fatalf("Error starting server: %v", err)
+		}
+	}()
+
+	fmt.Println("Server is running on http://localhost:8080")
+
+	//Interrupt signal
+	<-stop
+
+	fmt.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
+
+	fmt.Println("Server gracefully stopped")
+}
